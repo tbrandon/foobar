bbec63f546e3829910ed992d9a5bc3b0918d0b53
Tyler Brandon <tbrandon@gmail.com>
2017-06-11 19:53:28 +0200

    See if the default Travis VM/Container can handle a socat call.

--- foobar_test.go
+++ foobar_test.go
@@ -1,6 +1,10 @@
 package foobar
 
-import "testing"
+import (
+	"log"
+	"os/exec"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	s := Add(1, 2)
@@ -10,6 +14,16 @@ func TestAdd(t *testing.T) {
 }
 
 func TestAdd2(t *testing.T) {
+	cmd := exec.Command("socat",
+		"pty,raw,echo=0,link=/dev/shm/foo",
+		"pty,raw,echo=0,link=/dev/shm/bar")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cmd.Wait()
+	defer cmd.Process.Kill()
+
 	s := Add(3, 4)
 	if s != 7 {
 		t.Errorf("exptect 7, got %v\n", s)
